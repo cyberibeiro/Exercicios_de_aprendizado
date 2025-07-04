4fdf28bab1d5142a7be5dda5b4e4c4dd1dc1108e
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-04 12:41:45 -0300

    fazendo_exercicio_3

---
+++ src/exercicios_de_aprendizado/exercicio_3.py
@@ -0,0 +1,37 @@
+# f(n) = f(n-1) + f(n-2)
+# f(1) = 1
+# f(2) = 1
+# f(3) = f(2) + f(1)
+# f(3) = 1 + 1
+# f(3) = 2
+def fibonacci(n):
+    if 1 == n or 2 == n:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+def exercicio_3_ruim(limit):
+    pares = []
+    n = 1
+    fn = fibonacci(n)
+
+    while fn <= limit:
+        fn = fibonacci(n)
+        n = n + 1
+        if fn % 2 == 0:
+            pares.append(fn)
+
+    return sum(pares)
+
+def exercicio_3(limit):
+    novo_termo = []
+    f = 1
+    fn = 2
+
+    while fn <= limit:
+        if fn % 2 == 0:
+            novo_termo.append(fn)
+        f, fn = fn, f + fn
+
+    return novo_termo
+
+print("Soma dos pares:", exercicio_3_ruim(4_000_000))
