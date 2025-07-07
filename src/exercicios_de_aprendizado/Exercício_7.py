b70a82490c4bd81cdb40b88004a838a1fb5231d5
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-07 08:45:35 -0300

    exercise 7

--- src/exercicios_de_aprendizado/Exercício_7.py
+++ src/exercicios_de_aprendizado/Exercício_7.py
@@ -1,14 +1,13 @@
-def find_the_largest_palindrome(inicio, fim):
+def find_the_largest_palindrome(beginning, end):
     largest_palindrome = 0
-    for n1 in range(inicio, fim + 1):
-        for n2 in range(inicio, fim + 1):
+    for n1 in range(beginning, end + 1):
+        for n2 in range(beginning, end + 1):
             product = n1 * n2
             str_product = str(product)
             if str_product == str_product[::-1]:
                 if product > largest_palindrome:
                     largest_palindrome = product
     return largest_palindrome
-
 
 result = find_the_largest_palindrome(100, 999)
 print(f"O maior palíndromo é: {result}")
