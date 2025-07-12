cb3f42a8187017fe0dcf062f20bed3543546683b
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-12 17:22:41 -0300

    exercise 5 test

--- src/exercicios_de_aprendizado/exercise_5.py
+++ src/exercicios_de_aprendizado/exercise_5.py
@@ -1,12 +1,12 @@
-n = 600851475143
-x = 2
-factors = []
+def biggest_prime_factor(n: int) -> int:
+    x = 2
+    factors = []
 
-while x <= n:
-    if n % x == 0:
-        factors.append(x)
-        n = n // x
-    else:
-        x += 1
+    while x <= n:
+        if n % x == 0:
+            factors.append(x)
+            n = n // x
+        else:
+            x += 1
 
-print(f'O maior fator primo do número', n, 'é:', factors[-1])
+    return factors[-1]
