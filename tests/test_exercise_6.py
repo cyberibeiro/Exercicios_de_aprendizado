4ca0599e541850297fc438f090696aad046ae2d9
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-14 18:56:42 -0300

    test_6

--- tests/test_exercise_6.py
+++ tests/test_exercise_6.py
@@ -1,25 +1,34 @@
-from space_age import SpaceAge 
+from exercicios_de_aprendizado.exercise_6 import *
 
-def test_on_earth() -> None:
-    assert SpaceAge(1000000000).on_earth() == 31.69
 
-def test_on_mercury() -> None:
-    assert SpaceAge(2134835688).on_mercury() == 280.88
+def test_append() -> None:
+    assert append([1, 2], [3, 4]) == [1, 2, 3, 4]
 
-def test_on_venus() -> None:
-    assert SpaceAge(189839836).on_venus() == 9.78
 
-def test_on_mars() -> None:
-    assert SpaceAge(2329871239).on_mars() == 39.25
+def test_concatenate() -> None:
+    assert concatenate([1], [2, 3], [4]) == [1, 2, 3, 4]
 
-def test_on_jupiter() -> None:
-    assert SpaceAge(901876382).on_jupiter() == 2.41
 
-def test_on_saturn() -> None:
-    assert SpaceAge(3000000000).on_saturn() == 3.23
+def test_filter() -> None:
+    assert filter(lambda x: x > 2, [1, 2, 3, 4]) == [3, 4]
 
-def test_on_uranus() -> None:
-    assert SpaceAge(3210123456).on_uranus() == 1.21
 
-def test_on_neptune() -> None:
-    assert SpaceAge(8210123456).on_neptune() == 1.58
+def test_length() -> None:
+    assert length([9, 8, 7]) == 3
+    assert length(["Ana", "Bruno", "Clara", "Daniel"]) == 4
+
+
+def test_map() -> None:
+    assert map(lambda x: x * 2, [1, 2, 3]) == [2, 4, 6]
+
+
+def test_foldl() -> None:
+    assert foldl(lambda acc, x: x - acc, [1, 2, 3], 10) == -8
+
+
+def test_foldr() -> None:
+    assert foldr(lambda x, acc: acc - x, [1, 2, 3], 10) == 4
+
+
+def test_reverse() -> None:
+    assert reverse([1, 2, 3]) == [3, 2, 1]
