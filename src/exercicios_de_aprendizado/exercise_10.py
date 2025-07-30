d33249aceef2b92072e455b127a428c4a36249db
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-30 20:22:50 -0300

    I did the exercise_10 and your test.  learned about the module datetime and the your classes datetime and timedelta.

--- src/exercicios_de_aprendizado/exercise_10.py
+++ src/exercicios_de_aprendizado/exercise_10.py
@@ -1,1 +1,10 @@
 # Gigassegundo - https://exercism.org/tracks/python/exercises/gigasecond
+
+from datetime import datetime, timedelta  # O timedelta que define se vai ser segundos
+     #módulo         #classes
+class Gigasecond:
+    def __init__(self, moment: datetime) -> None: #Aqui diz que o tipo dele é uma data e hora
+        self.moment = moment
+
+    def date(self) -> datetime:
+        return self.moment + timedelta(seconds=1_000_000_000)
---
+++ tests/test_exercise_10.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from exercicios_de_aprendizado.exercise_10 import Gigasecond
+
+def test_date_exact_gigasecond() -> None:
+    start = datetime(2015, 1, 24, 22, 0, 0)
+    expected = datetime(2046, 10, 2, 23, 46, 40)
+    assert Gigasecond(start).date() == expected
+
+def test_date_only_date_given() -> None:
+    start = datetime(2000, 1, 1)
+    expected = datetime(2031, 9, 9, 1, 46, 40)
+    assert Gigasecond(start).date() == expected
+
+def test_recent_date() -> None:
+    start = datetime(2023, 7, 30, 15, 30, 0)
+    expected = datetime(2055, 4, 7, 17, 16, 40)
+    assert Gigasecond(start).date() == expected
