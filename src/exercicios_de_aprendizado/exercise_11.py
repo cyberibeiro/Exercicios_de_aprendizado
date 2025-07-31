9edd3ded49986a78f664ce8c79550935098e084b
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-07-30 22:34:26 -0300

    I did exercise 11 and the test

--- src/exercicios_de_aprendizado/exercise_11.py
+++ src/exercicios_de_aprendizado/exercise_11.py
@@ -1,1 +1,30 @@
 # Diferença da Soma Quadrada - https://projecteuler.net/problem=6
+
+def calculate_difference():
+
+sum_of_squares = 0
+n = 0
+sum_total = 0
+
+while n <= 100:
+    sum_of_squares += number**2
+    number += 1
+
+for x in range(101):
+    sum_total += x
+    square_of_sum = sum_total ** 2
+
+return square_of_sum - sum_of_squares
+
+'''
+Version interesting:
+
+sum_of_squares = sum(n**2 for n in range(1, 101))
+
+
+square_of_sum = sum(range(1, 101)) ** 2
+
+
+print(square_of_sum - sum_of_squares)
+
+'''
---
+++ tests/test_exercise_11.py
@@ -0,0 +1,6 @@
+from exercicios_de_aprendizado.exercise_11 import calculate_difference
+
+def test_calculate_difference() -> None:
+    expected = 25164150
+    assert calculate_difference() == expected
+
