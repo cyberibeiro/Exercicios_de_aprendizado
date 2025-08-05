cce04925953c0885959885a837535825cb007740
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-08-05 19:18:06 -0300

    I added tests in the test_rotational_cipher

--- tests/test_rotational_cipher.py
+++ tests/test_rotational_cipher.py
@@ -1,4 +1,10 @@
 from exercicios_de_aprendizado.rotational_cipher import caesar_cipher
 
-def test_caesar_cipher() -> None:
+def test_caesar_cipher_13() -> None:
     assert caesar_cipher("abcdefghijklmnopqrstuvwxyz", 13) == "nopqrstuvwxyzabcdefghijklm"
+
+def test_caesar_cipher_3() -> None:
+    assert caesar_cipher("abc", 3) == "def"
+
+def test_caesar_cipher_3_maiuscula() -> None:
+    assert caesar_cipher("Beatriz", 3) == "Endwulc"
